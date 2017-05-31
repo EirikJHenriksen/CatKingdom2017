0976519c7db87f68773d6d64e1829d83d4c9d42a
Eirik J. Henriksen <eirikjhen@gmail.com>
2017-05-31 11:11:27 +0200

    Cleaned up assets and fixed an error. Ready for testing before building lights.

--- Source/Gruppe7_FantasyGame/Gruppe7_FantasyGameGameMode.cpp
+++ Source/Gruppe7_FantasyGame/Gruppe7_FantasyGameGameMode.cpp
@@ -7,7 +7,7 @@
 AGruppe7_FantasyGameGameMode::AGruppe7_FantasyGameGameMode()
 {
 	// set default pawn class to our Blueprinted character
-	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/Blueprints/FantasyGameCharacter_BP"));
 	if (PlayerPawnBPClass.Class != NULL)
 	{
 		DefaultPawnClass = PlayerPawnBPClass.Class;
